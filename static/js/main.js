91f4bc9c58c9fbb9fbe4572ae944ac8f621538dd
rouwe <santosroweme14@gmail.com>
2022-09-05 07:23:46 +0800

    Created a function that assigns the product default preview source and some other functions that adds event listener to lightbox buttons

--- static/js/main.js
+++ static/js/main.js
@@ -8,8 +8,14 @@ prodDataPromise.then((getData) => {
     // 
     const {productSeller, productName, productDescription, priceInfo,
     previewSources} = getData["product"];
+    // Set default img source of product preview and lightbox preview
+    const defaultNumSrc = 1; // default preview source number
+    const defaultPreviewSrc = previewSources[`preview${defaultNumSrc}`]["full"];
+    Slider.setDefaultPreviewSrc("preview-img", defaultPreviewSrc);
     // Add event listener to preview buttons
     Slider.addSliderButtonListener("lightbox-showcase", "prev-btn", "next-btn", function () {
         console.log("Seller: ", productSeller, "|| Name: ", productName, "Caller: ", this);
     });
+    // Add event listener to lightbox close button
+    Slider.addCloseLightboxListener("lightbox-showcase", "close-lightbox-btn", function () {});
 });
--- static/js/preview-slider.js
+++ static/js/preview-slider.js
@@ -1,10 +1,10 @@
 function addSliderButtonListener(containerClass, prevClass, nextClass, clickHandler) {
     /*
     Add event listeners for slider buttons.
-        :@param parentClass: String - the class selector of the targeted buttons.
+        :@param containerClass: String - the class selector of the targeted elements container.
         :@param prevClass: String - the class selector of the prev button element.
         :@param nextClass: String - the class selector of the next button element.
-        :@param clickHandler: Function - the function to be used
+        :@param clickHandler: Function - the function to be used as event handler.
         :return undefined:
     */
     const lightBoxContainer = document.getElementsByClassName(containerClass)[0];
@@ -12,7 +12,30 @@ function addSliderButtonListener(containerClass, prevClass, nextClass, clickHand
     const nextButton = lightBoxContainer.getElementsByClassName(nextClass)[0];
     const sliderNavigator = [prevButton, nextButton];
     for (const navigator of sliderNavigator) {
-        navigator.addEventListener("click", clickHandler)
+        navigator.addEventListener("click", clickHandler);
+    }
+}
+function addCloseLightboxListener(containerClass, closeButtonClass, clickHandler) {
+    /*
+    Add event listeners for lightbox close button.
+        :@param containerClass: String - the class selector of the targeted element container.
+        :@param closeButtonClass: String - the class selector of the close button element.
+        :@param clickHandler: Function - the function be used as event handler.
+    */
+    const lightBoxContainer = document.getElementsByClassName(containerClass)[0];
+    const closeLightBoxButton = lightBoxContainer.getElementsByClassName(closeButtonClass)[0];
+    document.addEventListener("click", clickHandler);
+}
+function setDefaultPreviewSrc(previewImgClass, defaultPreviewSrc) {
+    /*
+    Sets the default image source of preview element
+        :@param previewImgClass: String - the class selector of the img elements.
+        :@param defaultPreviewSrc: String - the product first image source to be used as default.
+        :return undefined:
+    */
+    const previewImgArr = document.getElementsByClassName(previewImgClass);
+    for (const preview of previewImgArr) {
+        preview.setAttribute("src", defaultPreviewSrc); 
     }
 }
 function previewSlider(productPreviewSrcObj) {
@@ -22,4 +45,5 @@ function previewSlider(productPreviewSrcObj) {
         :return undefined:
     */
 }
-export { previewSlider, addSliderButtonListener }
+export { previewSlider, addSliderButtonListener, setDefaultPreviewSrc,
+    addCloseLightboxListener }
