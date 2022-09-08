fdd90447197e222e0b00762723fa00ae54b68a40
rouwe <santosroweme14@gmail.com>
2022-09-08 18:59:29 +0800

    Change the prepareToAdd function returned object value from innerHTML to textContent property

--- static/js/add-to-cart.js
+++ static/js/add-to-cart.js
@@ -1,14 +1,15 @@
 function prepareToAdd() {
     /*
-    Prepare all the neccesary data for adding to storage.
+    Prepare all the neccesary data for adding to cart.
+        :return orderObj: Object - contains the product data.
     */
-    const prodSeller = document.getElementsByClassName("prod-seller")[0];
-    const prodName = document.getElementsByClassName("prod-name")[0];
-    const prodDescription = document.getElementsByClassName("prod-description")[0];
-    const discountedPrice = document.getElementsByClassName("discounted-price")[0];
-    const discountPercentage = document.getElementsByClassName("discount-percentage")[0];
-    const originalPrice = document.getElementsByClassName("original-price")[0];
-    const orderQuantity = document.getElementsByClassName("order-quantity")[0];
+    const prodSeller = document.getElementsByClassName("prod-seller")[0].textContent;
+    const prodName = document.getElementsByClassName("prod-name")[0].textContent;
+    const prodDescription = document.getElementsByClassName("prod-description")[0].textContent;
+    const discountedPrice = document.getElementsByClassName("discounted-price")[0].textContent;
+    const discountPercentage = document.getElementsByClassName("discount-percentage")[0].textContent;
+    const originalPrice = document.getElementsByClassName("original-price")[0].textContent;
+    const orderQuantity = document.getElementsByClassName("order-quantity")[0].textContent;
     const orderObj = {
         prodSeller: prodSeller,
         prodName: prodName,
@@ -25,7 +26,5 @@ function addToCart() {
     Add product to cart.
     */
     const orderObj = prepareToAdd();
-    console.log(orderObj);
-    return false;
 }
 export { addToCart };
